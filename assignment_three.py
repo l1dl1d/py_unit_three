87fda01e6eb767b8c311f1e9ac96d11e09eeb056
l1dl1d <javeekh@ssfs.org>
2021-09-29 11:55:02 -0400

    unit 3 project almost finished

--- assignment_three.py
+++ assignment_three.py
@@ -1,5 +1,31 @@
+def function_purpose():
+    print("This program computes the surface area of a rectangular prism.")
+    print("Please input the length height, and width of the solid")
+    return
+length = float(input("please enter length:"))
+width = float(input("please enter width:"))
+height = float(input("please enter height:"))
+def area(l, w):
+    """
+    this functions calculates the area of a rectangle
+    :param l: this tells length
+    :param w: this tells width
+    :return: area of a rectangle
+    """
+    return l*w
+
 def surface_area(l, w, h):
-    top = l*w
-    bottom = l*w
-    left
-    surface_area = top*bottom*left*right*front*back
+    """
+    the function calculates the total surface area
+    :param l: this tells the length
+    :param w: this tells the width
+    :param h: this tells the height
+    :return: total surface area
+    """
+    top_bottom = area(l, w)*2
+    left_right = h*l*2
+    front_back = h*w*2
+    surface = float(top_bottom)+float(left_right)+float(front_back)
+    print("the surface area is", float(surface))
+
+surface_area(length, width, height)
