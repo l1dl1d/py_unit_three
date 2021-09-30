f547505b1a774032b5e3bcf401869706c7321b8a
l1dl1d <javeekh@ssfs.org>
2021-09-30 14:34:05 -0400

    unit 3 project finished

--- Tests/assignment_three_tests.py
+++ Tests/assignment_three_tests.py
@@ -4,8 +4,8 @@ import assignment_three
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
+        self.assertEqual(6, assignment_three.area(2,3))
+        self.assertEqual(8, assignment_three.area(2,4))
+        self.assertEqual(150, assignment_three.surface_area(4,3,9))
 if __name__ == '__main__':
     unittest.main()
--- assignment_three.py
+++ assignment_three.py
@@ -1,10 +1,19 @@
 def function_purpose():
+    """
+    Khalid. Last modified on 9/30/2021. Purpose of the program is to calculate the surface area of rectangular prisms.
+    :return:
+    """
     print("This program computes the surface area of a rectangular prism.")
     print("Please input the length height, and width of the solid")
-    return
-length = float(input("please enter length:"))
-width = float(input("please enter width:"))
-height = float(input("please enter height:"))
+def get_length():
+    length = float(input("please enter length:"))
+    return length
+def get_width():
+    width = float(input("please enter width:"))
+    return width
+def get_height():
+    height = float(input("please enter height:"))
+    return height
 def area(l, w):
     """
     this functions calculates the area of a rectangle
@@ -26,6 +35,13 @@ def surface_area(l, w, h):
     left_right = h*l*2
     front_back = h*w*2
     surface = float(top_bottom)+float(left_right)+float(front_back)
-    print("the surface area is", float(surface))
+    return surface
+def main():
+    function_purpose()
+    width = get_width()
+    height = get_height()
+    length = get_length()
+    print("the surface area is",surface_area(length, width, height) )
 
-surface_area(length, width, height)
+if __name__ == '__main__':
+    main()
